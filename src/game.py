362d1a69ae975c150be774b8ea57296ac41cd161
Mikkel Hooge Sørensen <Mikkel Hooge Srensen>
2019-01-21 00:31:03 +0100

    Volatile initial structure of program

---
+++ src/game.py
@@ -0,0 +1,10 @@
+"""
+---------------------------------------------------------------------------------
+game: Super class for game. Implements actions, evaluations, terminal tests, etc.
+---------------------------------------------------------------------------------
+"""
+class Game:
+    # Constructor
+    def __init__(self):
+        # Do stuff.
+        pass
---
+++ src/latrunculi.py
@@ -0,0 +1,11 @@
+"""
+---------------------------------------------------------------------
+latrunculi: Subclass of game. Implements game methods for latrunculi.
+---------------------------------------------------------------------
+"""
+from game import Game
+
+class Latrunculi(Game):
+    def __init__(self):
+        # Do stuff
+        Game.__init__(self)

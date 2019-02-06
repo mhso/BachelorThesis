f2d9f935411fd1bfa15d22dacacc93dc863785a0
Mikkel Hooge Sørensen <Mikkel Hooge Srensen>
2019-02-06 22:09:31 +0100

    Slight adjustment to iteration timing test

--- src/test_iteration_timing.py
+++ src/test_iteration_timing.py
@@ -16,31 +16,48 @@ RESET = "\033[0;37;40m"
 args = sys.argv
 
 test_iterations = int(args[1])
+games_to_test = None
+if len(args) > 2:
+    games_to_test = args[2]
 times_l = []
+times_s = []
 times_ne = []
 times_cf = []
+test_l = games_to_test is None or games_to_test == "latrunculi"
+test_ls = games_to_test is None or games_to_test == "latrunculi_s"
+test_lne = games_to_test is None or games_to_test == "latrunculi_ne"
+test_cf =  games_to_test is None or games_to_test == "connect4"
 
-print("\n{}-=-=-=- LATRUNCULI GAME TESTS -=-=-=-{}".format(YELLOW, RESET))
-for i in range(1, test_iterations+1):
-    print("Test iteration {}/{} ".format(i, test_iterations))
-    time_taken = test_latrunculi.run_iteration_timing_test(log_type="sql")
-    times_l.append(time_taken)
-print("\n{}-=-=-=- LATRUNCULI_S GAME TESTS -=-=-=-{}".format(YELLOW, RESET))
-for i in range(1, test_iterations+1):
-    print("Test iteration {}/{} ".format(i, test_iterations))
-    time_taken = test_latrunculi_s.run_iteration_timing_test(log_type="sql")
-    times_l.append(time_taken)
-print("\n{}-=-=-=- LATRUNCULI_NE GAME TESTS -=-=-=-{}".format(YELLOW, RESET))
-for i in range(1, test_iterations+1):
-    print("Test iteration {}/{} ".format(i, test_iterations))
-    time_taken = test_latrunculi_ne.run_iteration_timing_test(log_type=None)
-    times_ne.append(time_taken)
-print("\n{}-=-=-=-=- CONNECT FOUR TESTS -=-=-=-=-{}".format(YELLOW, RESET))
-for i in range(1, test_iterations+1):
-    print("Test iteration {}/{} ".format(i, test_iterations))
-    time_taken = test_cnnct_four.run_iteration_timing_test(log_type=None)
-    times_cf.append(time_taken)
+if test_l:
+    print("\n{}-=-=-=- LATRUNCULI GAME TESTS -=-=-=-{}".format(YELLOW, RESET))
+    for i in range(1, test_iterations+1):
+        print("Test iteration {}/{} ".format(i, test_iterations))
+        time_taken = test_latrunculi.run_iteration_timing_test(log_type="sql")
+        times_l.append(time_taken)
+if test_ls:
+    print("\n{}-=-=-=- LATRUNCULI_S GAME TESTS -=-=-=-{}".format(YELLOW, RESET))
+    for i in range(1, test_iterations+1):
+        print("Test iteration {}/{} ".format(i, test_iterations))
+        time_taken = test_latrunculi_s.run_iteration_timing_test(log_type="sql")
+        times_s.append(time_taken)
+if test_lne:
+    print("\n{}-=-=-=- LATRUNCULI_NE GAME TESTS -=-=-=-{}".format(YELLOW, RESET))
+    for i in range(1, test_iterations+1):
+        print("Test iteration {}/{} ".format(i, test_iterations))
+        time_taken = test_latrunculi_ne.run_iteration_timing_test(log_type=None)
+        times_ne.append(time_taken)
+if test_cf:
+    print("\n{}-=-=-=-=- CONNECT FOUR TESTS -=-=-=-=-{}".format(YELLOW, RESET))
+    for i in range(1, test_iterations+1):
+        print("Test iteration {}/{} ".format(i, test_iterations))
+        time_taken = test_cnnct_four.run_iteration_timing_test(log_type=None)
+        times_cf.append(time_taken)
 
-print("Average time for Latrunculi: {} s".format(sum(times_l) / len(times_l)))
-print("Average time for Latrunculi_ne: {} s".format(sum(times_ne) / len(times_ne)))
-print("Average time for Connect Four: {} s".format(sum(times_cf) / len(times_cf)))
+if test_l:
+    print("Average time for Latrunculi: {} s".format(sum(times_l) / len(times_l)))
+if test_ls:
+    print("Average time for Latrunculi_s: {} s".format(sum(times_s) / len(times_s)))
+if test_lne:
+    print("Average time for Latrunculi_ne: {} s".format(sum(times_ne) / len(times_ne)))
+if test_cf:
+    print("Average time for Connect Four: {} s".format(sum(times_cf) / len(times_cf)))
